c4a3ce3fe3c1a721b4829bec71f8e53b575b6ae2
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-27 16:34:06 +0800

    chore: 移除不必要的 semantic-release 插件，简化配置

--- release.config.mjs
+++ release.config.mjs
@@ -1,39 +1,6 @@
-import pkg from './package.json' with { type: 'json' }
-const { name } = pkg
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-    plugins: [
-        [
-            '@semantic-release/commit-analyzer',
-            {
-                config: './dist/index.mjs',
-            },
-        ],
-        [
-            '@semantic-release/release-notes-generator',
-            {
-                config: './dist/index.mjs',
-            },
-        ],
-        [
-            '@semantic-release/changelog',
-            {
-                changelogFile: 'CHANGELOG.md',
-                changelogTitle: `# ${name}`,
-            },
-        ],
-        '@semantic-release/npm',
-        '@semantic-release/github',
-        [
-            '@semantic-release/git',
-            {
-                assets: [
-                    'CHANGELOG.md',
-                    'package.json',
-                ],
-            },
-        ],
-    ],
+    extends: ['semantic-release-cmyr-config'],
 }
