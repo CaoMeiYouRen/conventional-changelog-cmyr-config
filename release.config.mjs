0d35e37b10f4c49c3512cde8a827c3f365b5cbbb
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-27 20:53:59 +0800

    fix: 修改插件发布配置以使用本地配置

--- release.config.mjs
+++ release.config.mjs
@@ -3,4 +3,18 @@
  */
 export default {
     extends: ['semantic-release-cmyr-config'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                config: './dist/index.mjs',
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                config: './dist/index.mjs',
+            },
+        ],
+    ],
 }
